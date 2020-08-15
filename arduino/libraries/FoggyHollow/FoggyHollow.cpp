e2f2dc9c3cd6c53be2cbdbb0f77341df2f42f8a7
Paul Morris <pmorris.tx@sbcglobal.net>
2020-08-14 19:37:57 -0500

    Working on Layout

--- arduino/libraries/FoggyHollow/FoggyHollow.cpp
+++ arduino/libraries/FoggyHollow/FoggyHollow.cpp
@@ -1,89 +1,255 @@
 #include "FoggyHollow.h"
-#include <LocoNet.h>
-FoggyHollowClass::FoggyHollowClass()
+#include "LocoNet.h"
+#include <SoftwareServo.h>
+
+FoggyHollowClass::FoggyHollowClass() {}
+
+/**
+ * Creates a light object using the specified Arduino pin to power an LED and the specified
+ * JMRI Sensor address (command) to switch the light on/off.
+ */
+fh_light FoggyHollowClass::createLight( uint8_t pin, uint8_t onCmd, uint8_t function, uint8_t functionCmd)
 {
+	fh_light light;
 
-}
+	light.pin = pin;
+	light.onCmd = onCmd;
+	light.isInverted = false;
+	light.function = function;
+	light.functionCmd = functionCmd;
+	light.isOn = false;
 
+	if (function == BLINKING)
+	{
+		light.rate1 = 30000;
+		light.rate2 = 30000;
+	}
+	else if (function == DIMMABLE)
+	{
+		light.rate1 = 1;
+		light.rate2 = 8;
+	}
+	else if (function == FLICKERING)
+	{
+		light.rate1 = 5;
+		light.rate2 = 40;
+	}
 
-LIGHT_DEF* FoggyHollowClass::createLight( int pin, int onCmd)
-{
-	LIGHT_DEF* light = new LIGHT_DEF;
+	pinMode(light.pin, OUTPUT);
 
-	light->pin = pin;
-	light->onCmd = onCmd;
-	light->function = ON_OFF;
-	light->isInverted = false;
-
-	pinMode(light->pin, OUTPUT);
-	light->isOn = false;
+	digitalWrite(light.pin, LOW);
+	LocoNet.reportSensor(light.onCmd, INACTIVE);
 
 	return light;
 }
 
-LIGHT_DEF* FoggyHollowClass::createFlickeringLight( int pin, int onCmd, int rate)
+fh_turnout FoggyHollowClass::createTurnout(uint8_t servoPin,  uint8_t turnoutAddr, uint8_t thrownIndicatorPin,  uint8_t thrownIndicatorAddr,  uint8_t closedIndicatorPin,  uint8_t closedIndicatorAddr)
 {
-	LIGHT_DEF* light = new LIGHT_DEF;
 
-	light->pin = pin;
-	light->onCmd = onCmd;
-	light->rate1 = 5;
-	light->rate2 = rate;
-	light->function = FLICKERING;
-	light->isFunctionOn = true;
-	light->isInverted = false;
+	fh_turnout turnout;
 
-	pinMode(light->pin, OUTPUT);
-	light->isOn = false;
+	turnout.servoPin = servoPin;
+	turnout.turnoutAddr = turnoutAddr;
+	turnout.thrownIndicatorAddr = thrownIndicatorAddr;
+	turnout.thrownIndicatorPin = thrownIndicatorPin;
+	turnout.closedIndicatorPin = closedIndicatorPin;
+	turnout.closedIndicatorAddr = closedIndicatorAddr;
 
-	return light;
+	// Defaults
+	turnout.closedPosition = 125;
+	turnout.thrownPosition = 75;
+	turnout.currentPosition = 100;
+
+    turnout.servoSlowdown = 3;   //servo loop counter limit
+	turnout.servoSlowCounter = 1; //servo loop counter to slowdown servo transit
+
+	turnout.hasCtcIndicators = false;
+
+	pinMode(turnout.servoPin, OUTPUT);
+
+	return turnout;
 }
 
-LIGHT_DEF* FoggyHollowClass::createDimmableLight(int pin, int onCmd, int dimCmd)
+/**
+ * Add addresses of CTC relay send/receive indicators to a turnout.
+ */
+void FoggyHollowClass::setCtcIndicators (fh_turnout *turnout, uint8_t sendAddr, uint8_t receiveAddr)
 {
-	LIGHT_DEF* light = new LIGHT_DEF;
-
-	light->pin = pin;
-	light->onCmd = onCmd;
-	light->function = DIMMABLE;
-	light->functionCmd = dimCmd;
-	light->isFunctionOn = false;
-	light->isInverted = false;
-	//light->rate1 = 20;
-	//light->rate2 = 160;
-	light->rate1 = 1;
-	light->rate2 = 8;
-
-	pinMode(light->pin, OUTPUT);
-	light->isOn = false;
-
-	return light;
+	turnout->ctcSendIndicatorAddr = sendAddr;
+	turnout->ctcReceiveIndicatorAddr = receiveAddr;
+	turnout->hasCtcIndicators = true;
 }
 
-LIGHT_DEF* FoggyHollowClass::createBlinkingLight(int pin, int onCmd, int blinkCmd)
+/**
+ * Change the position of a turnout....also deal with the on/off state of the turnout lantern.
+ */
+
+void FoggyHollowClass::changeTurnout(fh_turnout *turnout, uint8_t direction, SoftwareServo *turnoutServo)
 {
-	LIGHT_DEF* light = new LIGHT_DEF;
+	//
+	// Turn the CTC indicator N/R light off and set the increment for the correct
+	// direction
+	turnout->direction = direction;
+	if ((direction != THROWN))
+	{
+		turnout->increment = 1;
+		digitalWrite(turnout->thrownIndicatorPin, HIGH);
+		LocoNet.reportSensor(turnout->thrownIndicatorAddr, INACTIVE);
+	}
+	else if ((direction == THROWN))
+	{
+		turnout->increment = -1;
+		digitalWrite(turnout->closedIndicatorPin, HIGH);
+		LocoNet.reportSensor(turnout->closedIndicatorAddr, INACTIVE);
+	}
 
-	light->pin = pin;
-	light->onCmd = onCmd;
-	light->function = BLINKING;
-	light->functionCmd = blinkCmd;
-	light->isFunctionOn = false;
-	light->isInverted = false;
-	light->rate1 = 30000;
-	light->rate2 = 30000;
+	turnout->moving = true;
+	turnout->currentState = SENDING;
+	turnout->direction = direction;
+	turnoutServo->attach(turnout->servoPin);
 
-	pinMode(light->pin, OUTPUT);
-	light->isOn = false;
+	if (turnout->hasCtcIndicators)
+	{
+		LocoNet.reportSensor(turnout->ctcSendIndicatorAddr, ACTIVE);
+	}
 
-	return light;
+	//
+	//  If there is a relay attached to power the frog, turn power
+	//  to the frog off & report to JMRI
+	if (turnout->frogRelayThrownPin > 0)
+	{
+		digitalWrite(turnout->frogRelayThrownPin, HIGH);
+		digitalWrite(turnout->frogRelayClosedPin, HIGH);
+		LocoNet.reportSensor(turnout->frogRelayThrownAddr, INACTIVE);
+		LocoNet.reportSensor(turnout->frogRelayClosedAddr, INACTIVE);
+	}
+}
+
+/**
+ * moveTurnout
+ *
+ * Called repeatedly by the main Arduino loop to advance the turnout servo one degree.  We
+ * also control the software CTC relay lights.
+ *
+ */
+void FoggyHollowClass::moveTurnout(fh_turnout *turnout, SoftwareServo *turnoutServo)
+{
+	if (turnout->currentState == SENDING && turnout->hasCtcIndicators)
+	{
+		//
+		//  If we have CTC send/receive indicators, turn the SEND indicator off
+		//  and turn the receive indicator on.
+		LocoNet.reportSensor(turnout->ctcSendIndicatorAddr, ACTIVE);
+		if (turnout->ctcReceiveCount++ > 1000)
+		{
+			turnout->ctcReceiveCount = 0;
+			LocoNet.reportSensor(turnout->ctcSendIndicatorAddr, INACTIVE);
+			LocoNet.reportSensor(turnout->ctcReceiveIndicatorAddr, INACTIVE);
+			turnout->currentState = MOVING;
+		}
+	}
+	else if (turnout->currentState == MOVING)
+	{
+#ifdef DEBUG
+		Serial.println("in FoggyHollow::moveTurnout: ");
+		Serial.print ("currentPosition: ");
+		Serial.print (turnout->currentPosition);
+		Serial.print (" increment: ");
+		Serial.print (turnout->increment);
+
+		Serial.print (" closedPosition: ");
+		Serial.print (turnout->closedPosition);
+
+		Serial.print (" thrownPosition: ");
+		Serial.print (turnout->thrownPosition);
+
+		Serial.print (" servoSlowCounter: ");
+		Serial.print (turnout->servoSlowCounter);
+		Serial.print (" slowdown ");
+		Serial.println (turnout->servoSlowdown);
+#endif
+		//
+		//  Don't do anything until we've been through "serverSlowdown" times.
+		if (turnout->servoSlowCounter++ > turnout->servoSlowdown)
+		{
+			turnoutServo->write(turnout->currentPosition);
+			turnout->servoSlowCounter = 0;
+
+			turnout->currentPosition = turnout->currentPosition + turnout->increment;
+			if (turnout->increment > 0) {
+				if (turnout->currentPosition > turnout->closedPosition)
+				{
+					turnout->currentPosition = turnout->closedPosition;
+					LocoNet.reportSensor(turnout->ctcReceiveIndicatorAddr, ACTIVE);
+					turnout->currentState = RECEIVING;
+					turnout->moving = false;
+					turnoutServo->detach();
+				}
+			}
+			if (turnout->increment < 0) {
+				if (turnout->currentPosition < turnout->thrownPosition)
+				{
+					turnout->currentPosition = turnout->thrownPosition;
+					LocoNet.reportSensor(turnout->ctcReceiveIndicatorAddr, ACTIVE);
+					turnout->currentState = RECEIVING;
+					turnout->moving = false;
+					turnoutServo->detach();
+				}
+			}
+		}
+	}
+	else if (turnout->currentState == RECEIVING)
+	{
+		//
+		// Wait a couple of seconds then turn off the CTC Receive Indicator and
+		// turn on the turnout THROWN or CLOSED light.  We activate the Arduino
+		// pin to turn on the LED on the 'real' control panel and set the JMRI
+		// sensor to display on the 'software' control panel.
+		//
+		if (turnout->ctcReceiveCount++ > 1000)
+		{
+			LocoNet.reportSensor(turnout->ctcReceiveIndicatorAddr, INACTIVE);
+			if (turnout->direction != THROWN)
+			{
+				LocoNet.reportSensor(turnout->closedIndicatorAddr, ACTIVE);
+				digitalWrite(turnout->closedIndicatorPin, LOW);
+			}
+			else
+			{
+				LocoNet.reportSensor(turnout->thrownIndicatorAddr, ACTIVE);
+				digitalWrite(turnout->thrownIndicatorPin, LOW);
+			}
+
+			turnout->currentState = IDLE;
+			turnout->ctcReceiveCount = 0;
+		}
+	}
+	else
+	{
+		//
+		// Only fiddle with the frog controls if there is a relay attached to
+		// power the frog.
+		if (turnout->frogRelayClosedPin > 0)
+		{
+			if (turnout->direction != THROWN)
+			{
+				digitalWrite(turnout->frogRelayClosedPin, LOW);
+				LocoNet.reportSensor(turnout->frogRelayClosedAddr, ACTIVE);
+			}
+			else
+			{
+				digitalWrite(turnout->frogRelayThrownPin, LOW);
+				LocoNet.reportSensor(turnout->frogRelayThrownAddr, ACTIVE);
+			}
+		}
+	}
 }
 
 /**
  * Called from "loop" to blink an LED.  Note that we have to blink rates that allow
  * for dimming of an LED (pseudo PSM).
  */
-void FoggyHollowClass::blinkLED(LIGHT_DEF *light)
+void FoggyHollowClass::blinkLED(fh_light *light)
 {
 	//Serial.println ("Blinking LED");
 	if (light->counter++ > light->rate1 && light->phase == HIGH)
@@ -103,7 +269,7 @@ void FoggyHollowClass::blinkLED(LIGHT_DEF *light)
  * Sets the on/off state of a light.  We set the isBrightening/isDimming flags in the light
  * object, then let the "loop" fade the led to the requested state.
  */
-void FoggyHollowClass::setState(LIGHT_DEF *light, bool state)
+void FoggyHollowClass::setState(fh_light *light, bool state)
 {
 	light->counter = 0;
 	if (!light->isInverted)
@@ -138,7 +304,7 @@ void FoggyHollowClass::setState(LIGHT_DEF *light, bool state)
  * Set the state of a light "function" to on/off.  If we set the function to "on",
  * we also turn on the light
  */
-void FoggyHollowClass::setFunctionState(LIGHT_DEF *light, bool state)
+void FoggyHollowClass::setFunctionState(fh_light *light, bool state)
 {
 	light->counter = 0;
 	light->isFunctionOn = state;
@@ -163,34 +329,37 @@ void FoggyHollowClass::setFunctionState(LIGHT_DEF *light, bool state)
 	else
 	{
 		LocoNet.reportSensor(light->onCmd, INACTIVE);
-		LocoNet.reportSensor(light->functionCmd, INACTIVE);
+		if (light->functionCmd > 0)
+		{
+			LocoNet.reportSensor(light->functionCmd, INACTIVE);
+		}
 	}
 }
 
-void FoggyHollowClass::fadeLED(LIGHT_DEF *light, uint8_t requestedState)
+void FoggyHollowClass::fadeLED(fh_light light, uint8_t requestedState)
 {
-	  int fade_time = 170;
+	  uint8_t fade_time = 170;
 	  float time_fraction;
-	  int del_temp;
+	  uint8_t del_temp;
 
 	  //
 	  //  Only do this if we are changing the state of the light....don't do anything if
 	  //  the light is already in the requested state.
-	  if ((requestedState != INACTIVE && !light->isOn) || (requestedState == INACTIVE && light->isOn))
+	  if ((requestedState != INACTIVE && !light.isOn) || (requestedState == INACTIVE && light.isOn))
 	  {
-		  boolean start_state = light->isOn ? HIGH : LOW;
-		  boolean end_state = light->isOn? LOW : HIGH;
+		  boolean start_state = light.isOn ? HIGH : LOW;
+		  boolean end_state = light.isOn ? LOW : HIGH;
 
-		  for (int loop_time=0; loop_time<fade_time; loop_time++)  {
+		  for ( uint8_t loop_time=0; loop_time<fade_time; loop_time++)  {
 				time_fraction = (float (loop_time))/(float (fade_time));
-				digitalWrite (light->pin, start_state);
+				digitalWrite (light.pin, start_state);
 				del_temp = 1000 - (1000.*time_fraction);
 				if (del_temp<0) del_temp=0;
 				delayMicroseconds (del_temp);
-				digitalWrite (light->pin, end_state);
+				digitalWrite (light.pin, end_state);
 				delayMicroseconds (1000.*time_fraction);
 				}
-		  light->isOn = requestedState != INACTIVE;
+		  light.isOn = requestedState != INACTIVE;
 	  }
 }
 
@@ -198,38 +367,36 @@ void FoggyHollowClass::fadeLED(LIGHT_DEF *light, uint8_t requestedState)
  * Fades a light on/off.  Note that this method will execute until the light has
  * transitioned...may cause other lights to quit blinking/flickering/dimming.
  */
-void FoggyHollowClass::fadeOnOff(LIGHT_DEF *light, uint8_t state)
+void FoggyHollowClass::fadeOnOff(fh_light light, uint8_t state)
 {
 #define fadedelay 24
 #define fadeStep 12
 
-	int lightPin = light->pin;
-
-	if (state != LOW && !light->isOn)
+	if (state != LOW && !light.isOn)
 	{
 		// Turning on
-		for (int t = 0; t < fadeStep; t += 1)
+		for ( uint8_t t = 0; t < fadeStep; t += 1)
 		{
-			digitalWrite( lightPin, HIGH);
+			digitalWrite( light.pin, HIGH);
 			delay(fadedelay * (t / fadeStep));
-			digitalWrite( lightPin, LOW);
+			digitalWrite( light.pin, LOW);
 			delay(fadedelay - (fadedelay * (t / fadeStep)));
 		}
-		digitalWrite( lightPin,  HIGH );
-		light->isOn = true;
-		LocoNet.reportSensor(light->onCmd, ACTIVE);
-	} else if (state == LOW && light->isOn) {
+		digitalWrite( light.pin,  HIGH );
+		light.isOn = true;
+		LocoNet.reportSensor(light.onCmd, ACTIVE);
+	} else if (state == LOW && light.isOn) {
 		// Turning off
-		for (int t = 0; t < fadeStep; t += 1)
+		for ( uint8_t t = 0; t < fadeStep; t += 1)
 		{
-			digitalWrite( lightPin, LOW);
+			digitalWrite( light.pin, LOW);
 			delay(fadedelay * (t / fadeStep));
-			digitalWrite( lightPin, HIGH);
+			digitalWrite( light.pin, HIGH);
 			delay(fadedelay - (fadedelay * (t / fadeStep)));
 		}
-		digitalWrite(lightPin, LOW);
-		light->isOn = false;
-		LocoNet.reportSensor(light->onCmd, INACTIVE);
+		digitalWrite(light.pin, LOW);
+		light.isOn = false;
+		LocoNet.reportSensor(light.onCmd, INACTIVE);
 	}
 }
 
@@ -237,17 +404,17 @@ void FoggyHollowClass::fadeOnOff(LIGHT_DEF *light, uint8_t state)
  * Fades a light on/off based on the isBrightening/isDimming value.  This method is called within the
  * main loop to avoid locking up the Arduino (and pausing blink/flicker/dim on other lights).
  */
-void FoggyHollowClass::fadeOn(LIGHT_DEF *light)
+void FoggyHollowClass::fadeOn(fh_light *light)
 {
 #define fadedelay 24
 #define fadeStep 12
 
-	//if ((light->isBrightening && !light->isInverted) || (light->isDimming && light->isInverted))
+	//if ((light.isBrightening && !light.isInverted) || (light.isDimming && light.isInverted))
 	if (light->isBrightening)
 	{
 		//Serial.print ("Turning on light ");
 		//Serial.print (light->pin);
-		//Serial.print ("counter = ");
+		//Serial.print (" counter = ");
 		//Serial.println(light->counter);
 		// Turning on
 		digitalWrite( light->pin, HIGH);
@@ -261,14 +428,14 @@ void FoggyHollowClass::fadeOn(LIGHT_DEF *light)
 			light->isOn = true;
 			light->isBrightening = false;
 			light->isDimming = false;
-			LocoNet.reportSensor(light->onCmd, ACTIVE);
+			//LocoNet.reportSensor(light->onCmd, ACTIVE);
 		}
-//	} else if ((light->isDimming && !light->isInverted) || (light->isBrightening && light->isInverted)) {
+//	} else if ((light.isDimming && !light.isInverted) || (light.isBrightening && light.isInverted)) {
 	} else if (light->isDimming) {
 		// Turning off
 		//Serial.print ("Turning off light ");
 		//Serial.print (light->pin);
-		//Serial.print ("counter = ");
+		//Serial.print (" counter = ");
 		//Serial.println(light->counter);
 		digitalWrite( light->pin, LOW);
 		delay(fadedelay * (light->counter / fadeStep));
@@ -281,7 +448,7 @@ void FoggyHollowClass::fadeOn(LIGHT_DEF *light)
 			light->isDimming = false;
 			light->isBrightening = false;
 			light->isOn = false;
-			LocoNet.reportSensor(light->onCmd, INACTIVE);
+			//LocoNet.reportSensor(light->onCmd, INACTIVE);
 		}
 	}
 }
@@ -289,11 +456,11 @@ void FoggyHollowClass::fadeOn(LIGHT_DEF *light)
 /**
  *  Randomly flickers the light.  The speed of the flicker is determined by the rate2 value.  Typical value is 50.
  */
-void FoggyHollowClass::flickerLED(LIGHT_DEF *light)
+void FoggyHollowClass::flickerLED(fh_light *light)
 {
 	if (light->isOn)
 	{
-		int phase = LOW;
+		uint8_t phase = LOW;
 		if (light->counter++ > light->rate1)
 		{
 			light->counter = 0;
@@ -313,9 +480,9 @@ void FoggyHollowClass::flickerLED(LIGHT_DEF *light)
 /**
  * Called repeatedly from Arduino loop for each light.  Handles fade on/off, blink, dim, flicker.
  */
-void FoggyHollowClass::loop(LIGHT_DEF *light)
+void FoggyHollowClass::loop(fh_light *light)
 {
-		//Serial.println (light->pin);
+		//Serial.println (light.pin);
 		if (light->function == FLICKERING)
 		{
 			flickerLED(light);
@@ -329,16 +496,13 @@ void FoggyHollowClass::loop(LIGHT_DEF *light)
 			}
 			else if (!light->isOn)
 			{
-				//Serial.print ("Turning off light ");
 				digitalWrite(light->pin, LOW);
-				//Serial.print (light->pin);
 			}
 
 		}
 
 		if (light->isBrightening || light->isDimming)
 		{
-			//Serial.print ("Fading on");
 			fadeOn(light);
 		}
 
--- arduino/libraries/FoggyHollow/FoggyHollow.h
+++ arduino/libraries/FoggyHollow/FoggyHollow.h
@@ -1,34 +1,43 @@
+#ifndef FoggyHollow_h
+#define FoggyHollow_h
+
 #include "Arduino.h"
-#include "SoftwareServo.h"
+#include <SoftwareServo.h>
 
-const uint8_t ACTIVE = 16;
-const uint8_t INACTIVE = 0;
+	// Sensor states
+	const uint8_t ACTIVE = 16;
+	const uint8_t INACTIVE = 0; // Check this value....note value is the same as "THROWN" and "SWITCH_OFF"
 
-const uint8_t SWITCH_ON = 32; // Light ON
-const uint8_t SWITCH_OFF = 0; // Light OFF
+	// Turnout states
+	const uint8_t CLOSED = 32;
+	const uint8_t THROWN = 0;
 
-const int ON_OFF = 0;
-const int DIMMABLE = 1;
-const int BLINKING = 2;
-const int FLICKERING = 3;
+	// Light states
+	const uint8_t SWITCH_ON = 32; // Light ON
+	const uint8_t SWITCH_OFF = 0; // Light OFF
 
-	struct MY_SERVO_DEF
+	//
+	// Light functions
+	const uint8_t ON_OFF = 0;
+	const uint8_t DIMMABLE = 1;
+	const uint8_t BLINKING = 2;
+	const uint8_t FLICKERING = 3;
+
+	//
+	// Turnout states
+	const uint8_t IDLE = 0;		// Turnout is not moving...waiting for a command
+	const uint8_t SENDING = 1;  // CTC is sending request to turnout to move
+	const uint8_t MOVING = 2;	// Turnout is responding to CTC message to change
+	const uint8_t RECEIVING = 3; // Turnout is responding to CTC that it has changed
+
+	struct fh_light
 	{
-	//	String servoName;		// Name of the servo so we can identify the function
-		bool moving;				// Indicates that the servo is currently moving
-		bool finishedMoving = false;  	// Use to identify transition at end of move
-		int currentPosition;			// Current position of servo (angle)
-		int direction;					// Direction servo is moving (UP or DOWN)
-		int topPosition;// Position (angle) of servo when spout is UP (tank is full)
-		int bottomPosition;	// Position (angle) of servo when spout is DOWN (tank is empty)
-		int slowdown_rate;// Indicates how many times through LOOP to skip between moving servo - slows down movement
-		int slowdown_counter = 0;// Used with slowdown_rate to slow down movement...current counter
-		SoftwareServo servo;			// Servo to move
-	};
-
-	struct LIGHT_DEF
-	{
-		int pin = -1;
+		uint8_t pin = -1;		// Arduino pin controlling LED
+		uint8_t onCmd = -1;		// JMRI Sensor Address (command)
+		uint8_t function = 0; 	// Function type of this light.  0->onOff;
+		uint8_t functionCmd = -1; // JMRI Sensor Address of function command (dimming, etc.)
+		uint16_t rate1 = 2;		// Blink/Flicker rate 1
+		uint16_t rate2 = 2;		// Blink/Flicker rate 2
 
 		uint8_t currentIntensity = 0;
 		uint8_t maxIntensity = 255;
@@ -41,35 +50,66 @@ const int FLICKERING = 3;
 		bool isBrightening = false;
 		bool isPWM = false;
 		bool isFunctionOn = false;
-
 		bool isInverted = false;
 
-		int rate1 = 2;
-		int rate2 = 2;
-		int counter = 0;
-		int functionCmd = -1;
-		int onCmd = -1;
-		int phase = LOW;
-		int function = 0; // 0->onOff;
+		uint16_t counter = 0;
+		uint8_t phase = LOW;
 	};
 
-class FoggyHollowClass
-{
-	public:
-		FoggyHollowClass();
-		void fadeOnOff(LIGHT_DEF *light, uint8_t state);
-		void fadeLED(LIGHT_DEF *light, uint8_t requestedState);
-		void blinkLED(LIGHT_DEF *light);
-		void flickerLED(LIGHT_DEF *light);
-		void loop (LIGHT_DEF *light);
-		void fadeOn (LIGHT_DEF *light);
-		void setState(LIGHT_DEF *light, bool state);
-		void setFunctionState(LIGHT_DEF *light, bool state);
+	struct fh_turnout
+	{
+		uint8_t turnoutAddr;			// LocoNet turnout address
+		uint8_t servoPin;				// Arduino servo control pin
+		bool moving = false;			// Indicates that the servo is currently moving
+		int8_t currentPosition;			// Current position of servo (angle)
+		int8_t direction;				// Direction servo is moving
+		int8_t increment=1;				// Current position increment to move servo
+		uint8_t thrownPosition = 55;	// Position (angle) of servo
+		uint8_t closedPosition = 145;	// Position (angle) of servo
+		uint8_t currentState = IDLE;
 
-		LIGHT_DEF* createLight(int pin, int onCmd);
-		LIGHT_DEF* createBlinkingLight(int pin, int onCmd, int blinkCmd);
-		LIGHT_DEF* createDimmableLight(int pin, int onCmd, int dimCmd);
-		LIGHT_DEF* createFlickeringLight(int pin, int onCmd, int rate);
+        uint8_t servoSlowdown = 8;   	//servo loop counter limit
+		uint8_t servoSlowCounter = 1; 	//servo loop counter to slowdown servo transit
 
-};
-extern  FoggyHollowClass FoggyHollow;
+		uint8_t thrownIndicatorPin;		// Arduino pin to activate when turnout is THROWN
+		uint8_t thrownIndicatorAddr;	// Loconet address to report back to JMRI
+		uint8_t closedIndicatorPin;		// Arduino pin to activate when turnout is CLOSED
+		uint8_t closedIndicatorAddr;	// Loconet address to report back to JMRI
+
+		uint8_t frogRelayThrownAddr;
+		uint8_t frogRelayClosedAddr;
+		uint8_t frogRelayThrownPin;
+		uint8_t frogRelayClosedPin;
+
+		uint8_t ctcSendIndicatorAddr;		// JMRI Sensor Address of CTC Send Relay
+		uint8_t ctcReceiveIndicatorAddr;	// JMRI Sensor Address of CTC Receive Relay
+		bool hasCtcIndicators = false;
+		uint16_t ctcReceiveCount = 0;		// Counter used to keep CTC lights on
+	};
+
+	class FoggyHollowClass
+	{
+		public:
+			FoggyHollowClass();
+			void fadeOnOff(fh_light light, uint8_t state);
+			void fadeLED(fh_light light, uint8_t requestedState);
+			void blinkLED(fh_light *light);
+			void flickerLED(fh_light *light);
+			void loop (fh_light *light);
+			void fadeOn (fh_light *light);
+			void setState(fh_light *light, bool state);
+			void setFunctionState(fh_light *light, bool state);
+
+			fh_light createLight( uint8_t pin, uint8_t onCmd, uint8_t function, uint8_t functionCmd);
+
+			fh_turnout createTurnout(uint8_t servoPin, uint8_t turnoutAddr,
+					uint8_t thrownIndicatorAddr, uint8_t thrownIndicatorPin,
+					uint8_t closedIndicatorAddr, uint8_t closedIndicatorPin);
+
+			void changeTurnout(fh_turnout *turnout, uint8_t direction, SoftwareServo *turnoutServo);
+			void setCtcIndicators(fh_turnout *turnout, uint8_t sendAddr, uint8_t receiveAddr);
+			void moveTurnout(fh_turnout *turnout, SoftwareServo *turnoutServo);
+	};
+	extern  FoggyHollowClass FoggyHollow;
+
+#endif
